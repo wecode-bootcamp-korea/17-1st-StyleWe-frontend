da72b498ec4769a943e0534c19d7acd55c292896
ysh1394 <ysh13941394@gmail.com>
2021-02-15 18:04:01 +0900

    Add: Style 'We' 프로젝트 초기세팅 완료(프론트)

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route />
+        </Switch>
+      </Router>
+    );
+  }
+}
+export default Routes;
