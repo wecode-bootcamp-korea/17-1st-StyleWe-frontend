4c678c263d04c636ddcd413bde5983a1adc18457
ysh1394 <ysh13941394@gmail.com>
2021-02-17 16:37:48 +0900

    Add: 커뮤니티 메인 페이지 네비게이션바 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CommunityNav from "./Components/Nav/CommunityNav/CommunityNav";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route />
+          <Route exact path="/" component={CommunityNav} />
         </Switch>
       </Router>
     );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import "./Styles/commons.scss";
+import "./Styles/reset.scss";
 
 ReactDOM.render(
   <Routes />,
