d5a5cf6bb619f88cbab29831ed541a801034995f
PJH <carpediem.mementomori.1984@gmail.com>
2021-02-18 22:30:26 +0900

    ADD: Contents 업로드 페이지(컴포넌트) 및 기능 구현 완료

--- src/Pages/Community_Main/Uploadcontent.js
+++ src/Pages/Community_Main/Uploadcontent.js
@@ -2,21 +2,74 @@ import React, { Component } from 'react';
 import './Uploadcontent.scss';
 
 export default class UploadContent extends Component {
+  constructor() {
+    super();
+    this.state = { imgBase64: '', imgFile: null };
+  }
+
+  handleAttatchment = (event) => {
+    let reader = new FileReader();
+
+    reader.onloadend = (e) => {
+      const base64 = reader.result;
+      console.log(base64);
+
+      if (base64) {
+        this.setState({
+          imgBase64: base64.toString(),
+        });
+      }
+    };
+    if (event.target.files[0]) {
+      reader.readAsDataURL(event.target.files[0]);
+      this.setState({
+        imgFile: event.target.files[0],
+      });
+    }
+  };
+
+  handleRemove = () => {
+    this.setState({
+      imgBase64: '',
+      imgFile: null,
+    });
+  };
+
   render() {
     return (
       <div className="uploadContent">
-        <div class="mainBox">
+        <div className="mainBox">
           <form action="" method="POST">
-            <label for="imgFile">스타일 이미지</label>
+            <p>스타일 이미지</p>
+            {this.state.imgBase64 ? (
+              <>
+                <img src={this.state.imgBase64} alt="shownImage" />
+                <img
+                  src="https://www.flaticon.com/svg/vstatic/svg/1617/1617543.svg?token=exp=1613655420~hmac=4fb4fd9e6a908b37f25da129e5e81264"
+                  className="closeButton"
+                  onClick={this.handleRemove}
+                  alt="close"
+                />
+              </>
+            ) : (
+              <div></div>
+            )}
+            <label for="imgFile"></label>
             <input
               id="imgFile"
               type="file"
               accept="image/png, image/jpeg, image/jpg"
+              onChange={this.handleAttatchment}
               required
             />
             <hr />
-            <label for="">설명</label>
-            <textarea />
+            <p className="description">설명</p>
+            <textarea
+              id="description"
+              rows="5"
+              cols="97"
+              placeholder="내용을 입력해주세요"
+            />
             <hr />
             <button> 올리기 </button>
           </form>
