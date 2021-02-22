a93c22bb0232ba8d2f2554896bb2893156d8f9bc
ysh1394 <ysh13941394@gmail.com>
2021-02-22 19:28:18 +0900

    modified: 머지 후 Routes 경로 수정

--- src/Pages/Community_Main/CommnunityMain.js
+++ src/Pages/Community_Main/CommnunityMain.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import '../../Styles/commons.scss';
-import './CommunityMain.scss';
-import TopFeedSection from './TopFeedSection';
-import FeedUnit from './FeedUnit';
-import CreateModal from './CreateModal';
-import Footer from '../../Components/Footer/Footer';
+import React, { Component } from "react";
+import "../../Styles/commons.scss";
+import "./CommunityMain.scss";
+import TopFeedSection from "./TopFeedSection";
+import FeedUnit from "./FeedUnit";
+import CreateModal from "./CreateModal";
+import Footer from "../../Components/Footer/Footer";
+import CommunityNav from "../../Components/Nav/CommunityNav/CommunityNav";
 
 export default class CommnunityMain extends Component {
   constructor() {
@@ -13,14 +14,14 @@ export default class CommnunityMain extends Component {
   }
 
   getData = () => {
-    fetch('/data/mockFeedData.json')
+    fetch("/data/mockFeedData.json")
       .then((res) => res.json())
       .then((data) =>
         this.setState({
           feedContent: data,
         })
       );
-    fetch('/data/mockComments.json')
+    fetch("/data/mockComments.json")
       .then((res) => res.json())
       .then((data) =>
         this.setState({
@@ -46,52 +47,55 @@ export default class CommnunityMain extends Component {
   render() {
     const { feedContent, comment, isModalOpen } = this.state;
 
-    document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+    document.body.style.overflow = isModalOpen ? "hidden" : "auto";
 
     return (
-      <main className="CommunityMain">
-        <div className={'overlay ' + (isModalOpen && 'active')}></div>
-        <TopFeedSection />
-        <p className="sectionTitle">지금의 트랜드</p>
-        <div className="Feeds">
-          {feedContent.map((feed) => {
-            return (
-              <FeedUnit
-                className="FeedUnit"
-                key={feed.id}
-                username={feed.username}
-                img={feed.feedImages}
-                isLinked={feed.isLinkedProduct}
-                isProductInformation={feed.isProductInformation}
-                isCollected={feed.isCollected}
-                sns={feed.sns}
-                feedtext={feed.FeedText}
-                likedNumber={feed.LikedNumber}
-                comments={comment}
-              />
-            );
-          })}
-        </div>
-        <img
-          src="https://www.flaticon.com/svg/vstatic/svg/633/633773.svg?token=exp=1613537376~hmac=81fad313487f80917104ace958e971ce"
-          alt="up"
-          className="upScroll"
-          onClick={this.goUp}
-        />
-        <img
-          src="https://www.flaticon.com/svg/vstatic/svg/189/189689.svg?token=exp=1613538352~hmac=46f7b19be59420e6ad5cd995d78fa1ab"
-          alt="add"
-          className="createContent"
-          onClick={this.handleModal}
-        />
-        {isModalOpen && (
-          <CreateModal
-            handleModal={this.handleModal}
-            isModalOpen={isModalOpen}
+      <>
+        <CommunityNav />
+        <main className="CommunityMain">
+          <div className={"overlay " + (isModalOpen && "active")}></div>
+          <TopFeedSection />
+          <p className="sectionTitle">지금의 트랜드</p>
+          <div className="Feeds">
+            {feedContent.map((feed) => {
+              return (
+                <FeedUnit
+                  className="FeedUnit"
+                  key={feed.id}
+                  username={feed.username}
+                  img={feed.feedImages}
+                  isLinked={feed.isLinkedProduct}
+                  isProductInformation={feed.isProductInformation}
+                  isCollected={feed.isCollected}
+                  sns={feed.sns}
+                  feedtext={feed.FeedText}
+                  likedNumber={feed.LikedNumber}
+                  comments={comment}
+                />
+              );
+            })}
+          </div>
+          <img
+            src="https://www.flaticon.com/svg/vstatic/svg/633/633773.svg?token=exp=1613537376~hmac=81fad313487f80917104ace958e971ce"
+            alt="up"
+            className="upScroll"
+            onClick={this.goUp}
           />
-        )}
-        <Footer />
-      </main>
+          <img
+            src="https://www.flaticon.com/svg/vstatic/svg/189/189689.svg?token=exp=1613538352~hmac=46f7b19be59420e6ad5cd995d78fa1ab"
+            alt="add"
+            className="createContent"
+            onClick={this.handleModal}
+          />
+          {isModalOpen && (
+            <CreateModal
+              handleModal={this.handleModal}
+              isModalOpen={isModalOpen}
+            />
+          )}
+          <Footer />
+        </main>
+      </>
     );
   }
 }
--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CommunityNav from "./Components/Nav/CommunityNav/CommunityNav";
 import StoreNav from "./Components/Nav/StoreNav/StoreNav";
+import CommunityMain from "./Pages/Community_Main/CommnunityMain";
 
 class Routes extends React.Component {
   render() {
@@ -9,6 +10,8 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={CommunityMain} />
+          <Route exact path="/CommunityNav" component={CommunityNav} />
+          <Route exact path="/StoreNav" component={StoreNav} />
         </Switch>
       </Router>
     );
