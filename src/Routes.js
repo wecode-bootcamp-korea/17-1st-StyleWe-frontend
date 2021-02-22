4b4bf611b7131b132cce30c63cb0b8669af49b83
ysh1394 <ysh13941394@gmail.com>
2021-02-22 17:24:13 +0900

    Add: 스토어 네비게이션 바 구현 완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CommunityNav from "./Components/Nav/CommunityNav/CommunityNav";
+import StoreNav from "./Components/Nav/StoreNav/StoreNav";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={CommunityNav} />
+          <Route exact path="/" component={StoreNav} />
+          <Route exact path="/1" component={CommunityNav} />
         </Switch>
       </Router>
     );
